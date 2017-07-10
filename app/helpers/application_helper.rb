68275702be75c25e03e18078a1c827684328139d
Robert Travis Pierce <robert@roberttravispierce.com>
2017-07-09 22:44:51 -0500

    Update to ruby gems gem version and configure

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,3 @@
 module ApplicationHelper
 
-  def page_title(page_title)
-    content_for(:page_title) { page_title }
-  end
-
-  def set_page_title
-    Titler::Title.new(controller: self, i18n: I18n, title_as_set: content_for(:page_title) || @page_title).title
-  end
 end
